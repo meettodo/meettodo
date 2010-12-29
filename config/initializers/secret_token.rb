ad1428d0a8e09b5dd75472f599e2da08172899f2
jbandi <dev@jonasbandi.net>
2010-12-29 01:32:28 +0100

    Creating the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meettodo::Application.config.secret_token = '0cd20411d18ed49a2df70bea4e0202dcee30c9cc608633e12bd85a108fc9a6a1e35f0c4875be24ed761c4fb2f8c4cdcdb68c94b9c799d1de68f4978d01879c27'
